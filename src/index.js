28c4cbd3d18a05061927ef70836b1a09a2cfb434
rkd12 <rahulkd1203@gmail.com>
2023-10-13 13:01:45 +0530

    setup for sequelize completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const setupAndStartServer = async () => {
   const app = express();
 
   app.listen(PORT, () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
     console.log(`server started at ${PORT}`);
   });
 };
